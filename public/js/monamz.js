4b3ef1c82e108127a752854de4e5fec46a0bdcd3
Farhan-sid <fs440920@gmail.com>
2024-05-31 17:40:13 +0500

    update

--- public/js/monamz.js
+++ public/js/monamz.js
@@ -3,6 +3,8 @@ var dn = document.querySelector(".dn");
 var cd = document.querySelector(".cd")
 var mo = document.querySelector(".mask-overlay")
 var body = document.querySelector("body")
+var mb = document.querySelector("menu-bar")
+var pf = document.querySelector(".popup_filter")
 sea.addEventListener("click",function(){
     dn.style.opacity = 1
     dn.style.visibility = 'visible'
@@ -17,4 +19,12 @@ cd.addEventListener("click",function(){
     mo.style.opacity = 0
     mo.style.visibility = 'hidden'
     body.style.overflowY = "visible"
+})
+
+mb.addEventListener("click",function(){
+    pf.style.opacity = 1
+    pf.style.visibility = 'visible'
+    mo.style.opacity = 1
+    mo.style.visibility = 'visible'
+    body.style.overflowY = "hidden"
 })
